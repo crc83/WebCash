bc8307e166ab80878294c00b03224c5176d26178
Sergiy Beley <Sergiy.Beley@gmail.com>
2012-12-20 01:29:29 +0200

    performs only integration tests
    1) tomcat starts
    2) integration tests run
    3) I don't know if war file is deployed correctly
    
    Signed-off-by: Sergiy Beley <Sergiy.Beley@gmail.com>

--- webcash-spec/src/test/java/com/siriusif/ui/LoginPageTest.java
+++ webcash-spec/src/test/java/com/siriusif/ui/LoginPageTest.java
@@ -12,6 +12,7 @@ public class LoginPageTest {
 	@Test
 	public void testLoginPageOnRootPath() {
 		WebDriver driver = new HtmlUnitDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
 		// Find the text input element by its name
 		driver.get("http://localhost:8080/WebCash");
 		assertNotNull(driver.findElement(By.id("loginForm:username")));
