fd2344c59dd61514497a3d1ba35328bf9294e21f
colya <colya.s.o@gmail.com>
2012-12-19 22:48:35 +0200

    authorization login password. Don't work transition from login page to hall page
    Signed-off-by: colya <colya.s.o@gmail.com>

--- webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
+++ webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
@@ -8,11 +8,10 @@ import javax.faces.bean.ManagedBean;
 import org.primefaces.context.RequestContext;
 
 @ManagedBean(name = "loginBean")
-public class LoginBean {
+public class LoginBean{
 	private String username;  
+    private String password;
     
-    private String password;  
-      
     public String getUsername() {  
         return username;  
     }  
@@ -29,22 +28,26 @@ public class LoginBean {
         this.password = password;  
     }  
   
+    //private String url;
+    
     public String login(ActionEvent actionEvent) {
+    	String responce = "error";
         RequestContext context = RequestContext.getCurrentInstance();
         FacesMessage msg = null;
         boolean loggedIn = false;
        
         if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
                 loggedIn = true;
-                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+//                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
+                responce = "success";
         } else {
                 loggedIn = false;
                 msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");
         }
-       
-        FacesContext.getCurrentInstance().addMessage(null, msg);
+        
+        FacesContext.getCurrentInstance().addMessage("success", msg);
         context.addCallbackParam("loggedIn", loggedIn);
-		return password;
+		return responce;
 }
 
 }  
