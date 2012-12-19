8c55122c4eb989a241f693c7f17f4227d363b7ab
Sergiy Beley <Sergiy.Beley@gmail.com>
2012-12-19 02:06:40 +0200

    Add integration test
    Signed-off-by: Sergiy Beley <Sergiy.Beley@gmail.com>

---
+++ webcash-spec/src/test/java/com/siriusif/ui/LoginPageTest.java
@@ -0,0 +1,23 @@
+package com.siriusif.ui;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class LoginPageTest {
+
+	@Test
+	public void testLoginPageOnRootPath() {
+		WebDriver driver = new HtmlUnitDriver();
+		// Find the text input element by its name
+		driver.get("http://localhost:8080/WebCash");
+		assertNotNull(driver.findElement(By.id("loginForm:username")));
+		assertNotNull(driver.findElement(By.id("loginForm:password")));
+		//Close the browser
+        driver.quit();
+	}
+
+}
