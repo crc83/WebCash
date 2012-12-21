59f2667882917af253f21db2e30c59382999992b
Sergiy Beley <Sergiy.Beley@gmail.com>
2012-12-21 23:54:19 +0200

    [Issue #15] Specifications work in main project. Specifications should be located in *.integration.* packages
    Known issues:
    * Test server (Tomcat7) doesn't stop after tests
    
    Signed-off-by: Sergiy Beley <Sergiy.Beley@gmail.com>

--- webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
+++ webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
@@ -4,50 +4,69 @@ import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.faces.event.ActionEvent;
 import javax.faces.bean.ManagedBean;
+import javax.inject.Inject;
 
 import org.primefaces.context.RequestContext;
 
 @ManagedBean(name = "loginBean")
-public class LoginBean{
-	private String username;  
-    private String password;
-    
-    public String getUsername() {  
-        return username;  
-    }  
-  
-    public void setUsername(String username) {  
-        this.username = username;  
-    }  
-  
-    public String getPassword() {  
-        return password;  
-    }  
-  
-    public void setPassword(String password) {  
-        this.password = password;  
-    }  
-  
-    //private String url;
-    
-    public String login() {
-    	String responce = "/pages/login";
-        RequestContext context = RequestContext.getCurrentInstance();
-        FacesMessage msg = null;
-        boolean loggedIn = false;
-       
-        if(username != null && username.equals("admin") && password != null && password.equals("admin")) {
-                loggedIn = true;
-//                msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome", username);
-                responce = "/pages/hall?faces-redirect=true";
-        } else {
-                loggedIn = false;
-                msg = new FacesMessage(FacesMessage.SEVERITY_WARN, "Login Error", "Invalid credentials");
-        }
-        
+public class LoginBean {
+
+	@Inject
+	private FacesMessage message;
+
+	/** login from page */
+	private String username;
+
+	/** password from page */
+	private String password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public FacesMessage getMessage() {
+		return message;
+	}
+
+	public void setMessage(FacesMessage message) {
+		this.message = message;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * Action to check credentials and register user as active user
+	 * 
+	 * @return url to redirect after ok/wrong login
+	 */
+	public String login() {
+		String responce = "/pages/login";
+		boolean loggedIn = false;
+
+		if (username != null && username.equals("admin") && password != null
+				&& password.equals("admin")) {
+			loggedIn = true;
+			//TODO : Check how to test if we have messages
+			// message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome",
+			// username);
+			responce = "/pages/hall?faces-redirect=true";
+		} else {
+			loggedIn = false;
+			// message = new FacesMessage(FacesMessage.SEVERITY_WARN,
+			// "Login Error", "Invalid credentials");
+		}
+
 		return responce;
+	}
+
 }
-
-}  
-
-
---
+++ webcash/src/test/java/com/siriusif/integration/LoginPageTest.java
@@ -0,0 +1,24 @@
+package com.siriusif.integration;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+public class LoginPageTest {
+
+	@Test
+	public void testLoginPageOnRootPath() {
+		WebDriver driver = new HtmlUnitDriver();
+//		FirefoxDriver driver = new FirefoxDriver();
+		// Find the text input element by its name
+		driver.get("http://localhost:8080/WebCash");
+		assertNotNull(driver.findElement(By.id("loginForm:username")));
+		assertNotNull(driver.findElement(By.id("loginForm:password")));
+		//Close the browser
+        driver.quit();
+	}
+
+}
