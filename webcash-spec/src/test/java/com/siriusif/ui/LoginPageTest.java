f13b7b87ec14c31a9c7ec33162bc307bb1835983
Sergiy Beley <Sergiy.Beley@gmail.com>
2012-12-26 00:56:31 +0200

    Issue #15 : Integration tests now work as a separate project
    version changed from m1 to 0.1-SNAPSHOT for all artifacts
    
    Signed-off-by: Sergiy Beley <Sergiy.Beley@gmail.com>

--- webcash-spec/src/test/java/com/siriusif/ui/LoginPageTest.java
+++ webcash-spec/src/test/java/com/siriusif/ui/LoginPageTest.java
@@ -6,13 +6,16 @@ import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class LoginPageTest {
 
 	@Test
 	public void testLoginPageOnRootPath() {
 		WebDriver driver = new HtmlUnitDriver();
-//		FirefoxDriver driver = new FirefoxDriver();
+		//WebDriver driver = new ChromeDriver();
+		//WebDriver driver = new FirefoxDriver();
 		// Find the text input element by its name
 		driver.get("http://localhost:8080/webcash");
 		assertNotNull(driver.findElement(By.id("loginForm:username")));
