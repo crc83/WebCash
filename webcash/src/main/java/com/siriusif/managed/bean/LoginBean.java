ee5f0e7af445200f0ecc8e83d9b50c79182087af
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-01-11 02:02:04 +0200

    Issue #4 : Add Log4j support. Version of webcash updated to 0.2
    Signed-off-by: Sergiy Beley <Sergiy.Beley@gmail.com>

--- webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
+++ webcash/src/main/java/com/siriusif/managed/bean/LoginBean.java
@@ -3,9 +3,13 @@ package com.siriusif.managed.bean;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.inject.Inject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @ManagedBean(name = "loginBean")
 public class LoginBean {
+	
+	private static final Logger LOGGER = LoggerFactory.getLogger(LoginBean.class);
 
 	@Inject
 	private FacesMessage message;
@@ -47,14 +51,16 @@ public class LoginBean {
 	 */
 	public String login() {
 		String responce = "/pages/login";
-
+		LOGGER.debug("checking login and password");
 		if (username != null && username.equals("admin") && password != null
 				&& password.equals("admin")) {
 			//TODO : Check how to test if we have messages
 			// message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Welcome",
 			// username);
+			LOGGER.debug("correct user");
 			responce = "/pages/hall?faces-redirect=true";
 		} else {
+			LOGGER.debug("wrong user");
 			// message = new FacesMessage(FacesMessage.SEVERITY_WARN,
 			// "Login Error", "Invalid credentials");
 		}
