2085a879eb3d22735c004da4eead42cd4cc75865
colya <colya.s.o@gmail.com>
2013-01-12 17:14:13 +0200

    issue #22 closedAt of Workshift
    add to db not default closedAt
    add class helper in tests
    test closedAt set db
    test closedAt reading from db
    test openedAt notNull

--- webcash-domain/src/test/java/com/siriusif/service/model/WorkshiftDaoImplTest.java
+++ webcash-domain/src/test/java/com/siriusif/service/model/WorkshiftDaoImplTest.java
@@ -2,9 +2,12 @@ package com.siriusif.service.model;
 
 import static org.junit.Assert.*;
 
+import java.util.Date;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.siriusif.helper.Helper;
 import com.siriusif.model.Workshift;
 
 public class WorkshiftDaoImplTest extends AbstractDaoImplTest{
@@ -18,9 +21,54 @@ public class WorkshiftDaoImplTest extends AbstractDaoImplTest{
 		
 		Workshift workshift = new Workshift();
 		workshift.setDailyId(12);
+		workshift.setClosedAt(Helper.stringToDate("22/01/2013"));
 		workshiftDao.add(workshift);
 		
 		assertTrue (size < workshiftDao.list().size());
+	}
+
+	/**
+	 * We can't set up close date during creation of workshift !!!
+	 */
+	@Test
+	public void testEmptyCloseDateDuringInsert() {
+		Workshift workshift = new Workshift();
+		workshift.setDailyId(12);
+		workshift.setClosedAt(Helper.stringToDate("22/01/2013"));
+		workshiftDao.add(workshift);
+		// get this workshift from database
+		Workshift wsFroDB = workshiftDao.find(workshift.getId()); 
+		assertNull(wsFroDB.getClosedAt());
+	}
+
+	@Test
+	public void testSetCloseDate() {
+		Workshift workshift = new Workshift();
+		workshift.setDailyId(12);
+		workshiftDao.add(workshift);
+		// get this workshift from db
+		Date closedAt = Helper.stringToDate("22/01/2013");
+		workshift.setClosedAt(closedAt);
+
+		// NOTE: Workshift open date is generated on the db side.
+		// we can't update this value during update operations 
+		assertNull(workshift.getOpenedAt());
+		workshiftDao.update(workshift);
+		
+		// get this workshift from database
+		Workshift wsFroDB = workshiftDao.find(workshift.getId()); 
+		assertEquals(closedAt, wsFroDB.getClosedAt());
+	}
+
+	
+	@Test
+	public void testSetOpenDateNotNull() {
+		Workshift workshift = new Workshift();
+		workshift.setDailyId(12);
+		workshiftDao.add(workshift);
+		// get this workshift from database
+		Workshift wsFroDB = workshiftDao.find(workshift.getId()); 
+		assertNotNull(wsFroDB.getOpenedAt());
 	}
 
 }
