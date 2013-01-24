162d256b111b6f06b686737832f10d0c469319fe
colya <colya.s.o@gmail.com>
2013-01-24 15:48:50 +0200

    issue #18 hall/tables many-to-one doesn't work

--- webcash-domain/src/main/java/com/siriusif/model/Hall.java
+++ webcash-domain/src/main/java/com/siriusif/model/Hall.java
@@ -2,25 +2,30 @@ package com.siriusif.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "hall")
 public class Hall {
 	@Id
-	@GeneratedValue(strategy = GenerationType.TABLE)
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="hallIdi")
 	private Long id;
 	
 	@Column(name="name", nullable=false, length=100)
 	private String name;
-//	private List<TablesHall> tables;
-//	
+	
+	@OneToMany(mappedBy="hall")
+	private Set<TablesHall> tables;
+	
 //	public Hall(){
 //		tables = new ArrayList<TablesHall>();
 //	}
@@ -45,11 +50,11 @@ public class Hall {
 	public void setName(String name) {
 		this.name = name;
 	}
-//	public List<TablesHall> getTables() {
-//		return tables;
-//	}
-//	public void setTables(List<TablesHall> tables) {
-//		this.tables = tables;
-//	}
+	public Set<TablesHall> getTables() {
+		return tables;
+	}
+	public void setTables(Set<TablesHall> tables) {
+		this.tables = tables;
+	}
 
 }
