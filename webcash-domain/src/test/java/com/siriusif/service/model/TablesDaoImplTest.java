d41680a4f8f9094cb733957466115e05bc337c5f
colya <colya.s.o@gmail.com>
2013-01-29 18:06:54 +0200

    issue #31 add table.json and write to db

--- webcash-domain/src/test/java/com/siriusif/service/model/TablesDaoImplTest.java
+++ webcash-domain/src/test/java/com/siriusif/service/model/TablesDaoImplTest.java
@@ -2,9 +2,17 @@ package com.siriusif.service.model;
 
 import static org.junit.Assert.*;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import com.google.gson.Gson;
 import com.siriusif.model.TablesHall;
 
 public class TablesDaoImplTest extends AbstractDaoImplTest{
@@ -26,6 +34,21 @@ public class TablesDaoImplTest extends AbstractDaoImplTest{
 		tablesDao.add(tables);
 		
 		assertTrue (size < tablesDao.list().size());
+	}
+	
+	private BufferedReader getCPFileReader(String fileName)
+			throws UnsupportedEncodingException {
+		InputStream in = this.getClass().getResourceAsStream(fileName);
+		Reader reader = new InputStreamReader(in, "UTF-8");
+		BufferedReader bufferedReader = new BufferedReader(reader);
+		return bufferedReader;
+	}
+
+	@Test
+	public void testAddJsonToDb()throws IOException{		
+		TablesHall tablesHall = new Gson().fromJson(getCPFileReader("/tables.json"), TablesHall.class);
+		tablesDao.add(tablesHall);
+		
 	}
 
 }
