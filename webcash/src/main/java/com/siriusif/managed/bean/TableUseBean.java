d365000d95d32c65388a4943244a3136ea591e3d
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-02-02 01:40:06 +0200

    Issue #31 : Table rendered

--- webcash/src/main/java/com/siriusif/managed/bean/TableUseBean.java
+++ webcash/src/main/java/com/siriusif/managed/bean/TableUseBean.java
@@ -5,6 +5,7 @@ import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;  
 import java.io.ByteArrayOutputStream;  
 import java.io.File;  
+import java.io.IOException;
 import java.util.List;
 
 import javax.faces.bean.ManagedBean;
@@ -52,65 +53,31 @@ public class TableUseBean {
 	}
 
 	public TableUseBean() {
-        try {
-        	LOGGER.debug(" >> TablesDao is null? "+(tablesDao==null));
-        	List<TablesHall> tables = tablesDao.list();
-        	TablesHall tablesHall = tables.get(0);
-      
-        	//Graphic Text 
-        	  File imageSrc = null;
-        	  String tablesImageURL = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/images/table.png");
-            imageSrc = new File(tablesImageURL);
-            BufferedImage bi = ImageIO.read(imageSrc);
-        	BufferedImage bufferedImg = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);  
-             Graphics2D g2 = bufferedImg.createGraphics();  
-             g2.drawImage(bi, null, tablesHall.getLeft(), tablesHall.getTop());  
-             ByteArrayOutputStream os = new ByteArrayOutputStream();  
-             ImageIO.write(bufferedImg, "png", os);  
-             graphicText = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "image/png");   
-//            File imageSrc = null;
-//            imageSrc = new File("/images/tables.png");
-//        	BufferedImage img = ImageIO.read(imageSrc);
-//        	int w = img.getWidth(null);
-//        	int h = img.getHeight(null);
-//        	BufferedImage bi = new
-//        	    BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);
-//        	Graphics g = bi.getGraphics();
-//        	g.drawImage(img, 50, 50, null);
-//        	ByteArrayOutputStream os = new ByteArrayOutputStream();  
-//            ImageIO.write(bi, "png", os);  
-//        	
-//            graphicText = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "image/png");   
-//        	String path = "/images/table.png";
-//        	BufferedImage bufferedImg = new BufferedImage(100, 100, BufferedImage.TYPE_INT_ARGB);  
-//            Graphics2D g2 = bufferedImg.createGraphics();
-//            float[] scales = { 1f, 1f, 1f, 0.5f };
-//            float[] offsets = new float[4];
-//            RescaleOp rop = new RescaleOp(scales, offsets, null);
-//
-//            g2.drawImage(bufferedImg, rop, 10, 0);  
-//            ByteArrayOutputStream os = new ByteArrayOutputStream();  
-//            ImageIO.write(bufferedImg, path, os);  
-//            graphicText = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "image/png");   
-        	
-//            BufferedImage bufferedImg = new BufferedImag(10, 10, BufferedImage.TYPE_INT_RGB);  
-//            Graphics2D g2 = bufferedImg.createGraphics();  
-//            g2.drawImage(, 10, 10);  
-//            ByteArrayOutputStream os = new ByteArrayOutputStream();  
-//            ImageIO.write(bufferedImg, "png", os);  
-//            graphicText = new DefaultStreamedContent(getClass().getClassLoader().getResourceAsStream("/images/tables.png"), "image/png");   
-//            getClass().getClassLoader().getResourceAsStream(
-           
-        } catch (Exception e) {  
-            e.printStackTrace();  
-        }  
+		super();
+		//NOTE : Dao is not accessible here
     }  
       
     public StreamedContent getBarcode() {  
         return barcode;  
     }  
   
-    public StreamedContent getGraphicText() {  
+    public StreamedContent getGraphicText() throws IOException {  
+    	LOGGER.debug(" >> TablesDao is null? "+(tablesDao==null));
+    	List<TablesHall> tables = tablesDao.list();
+    	TablesHall tablesHall = tables.get(0);
+  
+    	//Graphic Text 
+    	  File imageSrc = null;
+    	  String tablesImageURL = FacesContext.getCurrentInstance().getExternalContext().getRealPath("/images/table.png");
+        imageSrc = new File(tablesImageURL);
+        BufferedImage bi = ImageIO.read(imageSrc);
+    	BufferedImage bufferedImg = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_RGB);  
+         Graphics2D g2 = bufferedImg.createGraphics();  
+         g2.drawImage(bi, null, tablesHall.getLeft(), tablesHall.getTop());  
+         ByteArrayOutputStream os = new ByteArrayOutputStream();  
+         ImageIO.write(bufferedImg, "png", os);  
+         graphicText = new DefaultStreamedContent(new ByteArrayInputStream(os.toByteArray()), "image/png");   
+
         return graphicText;  
     }  
           
