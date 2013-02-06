74d8e8f5499ea2a3e1e218507d630db2819f52c8
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-02-07 01:54:42 +0200

    Issue #33: Logger added for Spring. Properties fixed (iteration 1)

--- webcash-domain/src/test/java/com/siriusif/service/model/AbstractDaoImplTest.java
+++ webcash-domain/src/test/java/com/siriusif/service/model/AbstractDaoImplTest.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-@PropertySource("classpath*:test_db.properties")
+@PropertySource("classpath:/test_db.properties")
 @ContextConfiguration(locations = { "/webcash-persistance-beans.xml"})
 public abstract class AbstractDaoImplTest  extends AbstractJUnit4SpringContextTests{
 
