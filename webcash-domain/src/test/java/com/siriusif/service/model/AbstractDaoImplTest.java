4a85121b8033dd28c963416a3a412f6b33284cf1
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-02-09 09:19:27 +0200

    Tests work with inmemory database

--- webcash-domain/src/test/java/com/siriusif/service/model/AbstractDaoImplTest.java
+++ webcash-domain/src/test/java/com/siriusif/service/model/AbstractDaoImplTest.java
@@ -1,11 +1,9 @@
 package com.siriusif.service.model;
 
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 
-@PropertySource("classpath:/test_db.properties")
-@ContextConfiguration(locations = { "/webcash-persistance-beans.xml"})
+@ContextConfiguration(locations = { "/webcash-persistance-beans.xml", "/test-persistence-beans.xml"})
 public abstract class AbstractDaoImplTest  extends AbstractJUnit4SpringContextTests{
 
 }
