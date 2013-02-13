0bf75eac3481aeaff5fd4481e103f31d973256dd
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-02-13 02:48:05 +0200

    [Issue #34] Implemented draft solution

---
+++ webcash-domain/src/main/java/com/siriusif/ImportDatabase.java
@@ -0,0 +1,37 @@
+package com.siriusif;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.Properties;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.slf4j.Logger;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.google.gson.JsonIOException;
+import com.google.gson.JsonSyntaxException;
+import com.siriusif.helper.Helper;
+import com.siriusif.model.Hall;
+
+/**
+ * This console app allow us to create database with initial data
+ * for demonstration and testing
+ *
+ */
+public class ImportDatabase {
+	
+	public static void main(String[] args) throws JsonSyntaxException, JsonIOException, IOException {
+		Hall hall = Helper.fromJson("/demo_hall.json", Hall.class);
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("webcash-persistence-beans.xml");
+		SessionFactory sessionFactory = (SessionFactory) context.getBean("sessionFactory");
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(hall);
+		session.getTransaction().commit();
+		session.close();
+	}
+
+}
