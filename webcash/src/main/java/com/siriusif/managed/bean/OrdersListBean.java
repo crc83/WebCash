f19411f7df22f32227f4ab377605badf45706af8
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-04-07 23:37:05 +0300

    OPEN - task 59: Users from db : OrderListBean covered by tests http://github.com/crc83/WebCash/issues/issue/59

--- webcash/src/main/java/com/siriusif/managed/bean/OrdersListBean.java
+++ webcash/src/main/java/com/siriusif/managed/bean/OrdersListBean.java
@@ -11,6 +11,7 @@ import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -22,53 +23,72 @@ import com.siriusif.service.model.OrderDao;
 @ViewScoped
 public class OrdersListBean {
 
-	private static final Logger LOGGER = LoggerFactory.getLogger(OrdersListBean.class);
-	
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(OrdersListBean.class);
+
 	private long tableId;
-	
+
 	private List<Order> ordersForTable;
-	
-	//TODO SB : Remove access to dao clases here
-	@ManagedProperty(value="#{orderDao}")
+
+	// TODO SB : Remove access to dao clases here
+	@ManagedProperty(value = "#{orderDao}")
 	private OrderDao orderDao;
-	
-	@ManagedProperty(value="#{orderProcess}")
+
+	@ManagedProperty(value = "#{orderProcess}")
 	private OrderProcess orderProcess;
 
 	@PostConstruct
 	public void init() {
 		LOGGER.error("starting view");
-//		orderProcess.setCurrentUser(curentUser)
-		
-		HttpServletRequest request = (HttpServletRequest)FacesContext.getCurrentInstance().getExternalContext().getRequest();
-		String value=request.getParameter("table");
-		LOGGER.info("Recieved table id :"+value);
-		tableId = Long.parseLong(value);
-		if(skipAndOpenNewOrder()) {
-			try {
-				Order order = orderProcess.newOrder();
-				//since order list and order are at the same level in /pages
-				FacesContext.getCurrentInstance().getExternalContext().redirect("order.jsf?order_id="+order.getId());
-			} catch (IOException e) {
-				LOGGER.error("I can't open new order");
-				LOGGER.debug("Error while redirecting to new check", e);
+		// orderProcess.setCurrentUser(curentUser)
+
+		HttpServletRequest request = (HttpServletRequest) FacesContext
+				.getCurrentInstance().getExternalContext().getRequest();
+		String tableIdStr = request.getParameter("table");
+		redirectTo(urlToNewOrderIfNoOrdersForTable(tableIdStr));
+	}
+
+	public String urlToNewOrderIfNoOrdersForTable(String tableIdStr) {
+		String redirectTo ="";
+		LOGGER.info("Recieved table id :" + tableIdStr);
+		tableId = Long.parseLong(tableIdStr);
+		//Here is rule to open new order immediately
+		if (orderDao.countOpenedForTableId(tableId) < 1) {
+			Order order = orderProcess.newOrder();
+			if (order != null) {
+				// since order list and order are at the same level in /pages
+				redirectTo = "order.jsf?order_id=" + order.getId();
+			} else {
+				//order hasn't been created for some reason
+				notifyUser("I can't create new order");
+				LOGGER.error("I can't create new order");
 			}
 		}
+		return redirectTo;
 	}
 	
-	/**
-	 * Here is rule to open new order immediately
-	 * @return true if we should skip this page
-	 */
-	public boolean skipAndOpenNewOrder(){
-		return (orderDao.countOpenedForTableId(tableId)<1);
+	private void notifyUser(String message) {
+		//TODO SB : Implement
 	}
-	
+
+	private void redirectTo(String url) {
+		try {
+			if (StringUtils.isNotBlank(url)){
+				FacesContext.getCurrentInstance().getExternalContext().redirect(url);
+			}
+		} catch (IOException e) {
+			LOGGER.error("I can't open new order");
+			LOGGER.debug("Error while redirecting to new check", e);
+		}
+
+	}
+
 	/**
-	 * This method retrieves table_id from request parameter <code>table</code>.</br>
-	 * NOTE : This method stores retrieved table_id for future use.
-	 * @return table id for which order list should be displayed. 
-	 * At the moment this page invoked only from tables selection view.
+	 * This method retrieves table_id from request parameter <code>table</code>
+	 * .</br> NOTE : This method stores retrieved table_id for future use.
+	 * 
+	 * @return table id for which order list should be displayed. At the moment
+	 *         this page invoked only from tables selection view.
 	 */
 	public long getTableId() {
 		return tableId;
@@ -76,14 +96,14 @@ public class OrdersListBean {
 
 	public void setTableId(long tableId) {
 		this.tableId = tableId;
-	} 
-	
+	}
+
 	public List<Order> getOrdersForTable() {
 		return orderDao.listForTableId(getTableId());
 	}
-	
-	public void setOrdersForTable() {
-		//this is read only property
+
+	public void setOrdersForTable(List<Order> orders) {
+		// this is read only property
 	}
 
 	public OrderDao getOrderDao() {
