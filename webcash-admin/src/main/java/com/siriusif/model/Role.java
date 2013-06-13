7006222222f860163faa63eb8c90b0c99561d88f
Sergiy Beley <Sergiy.Beley@gmail.com>
2013-06-13 22:50:23 +0300

    add Role and Permission OPEN - task 112: make webcash-admin https://github.com/crc83/WebCash/issues/issue/112

---
+++ webcash-admin/src/main/java/com/siriusif/model/Role.java
@@ -0,0 +1,97 @@
+package com.siriusif.model;
+
+import javax.persistence.Entity;
+import java.io.Serializable;
+import javax.persistence.Id;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Column;
+import javax.persistence.Version;
+import java.lang.Override;
+
+@Entity
+public class Role implements Serializable
+{
+
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
+   @Column(name = "id", updatable = false, nullable = false)
+   private Long id = null;
+   @Version
+   @Column(name = "version")
+   private int version = 0;
+
+   @Column
+   private String name;
+
+   public Long getId()
+   {
+      return this.id;
+   }
+
+   public void setId(final Long id)
+   {
+      this.id = id;
+   }
+
+   public int getVersion()
+   {
+      return this.version;
+   }
+
+   public void setVersion(final int version)
+   {
+      this.version = version;
+   }
+
+   @Override
+   public boolean equals(Object that)
+   {
+      if (this == that)
+      {
+         return true;
+      }
+      if (that == null)
+      {
+         return false;
+      }
+      if (getClass() != that.getClass())
+      {
+         return false;
+      }
+      if (id != null)
+      {
+         return id.equals(((Role) that).id);
+      }
+      return super.equals(that);
+   }
+
+   @Override
+   public int hashCode()
+   {
+      if (id != null)
+      {
+         return id.hashCode();
+      }
+      return super.hashCode();
+   }
+
+   public String getName()
+   {
+      return this.name;
+   }
+
+   public void setName(final String name)
+   {
+      this.name = name;
+   }
+
+   @Override
+   public String toString()
+   {
+      String result = getClass().getSimpleName() + " ";
+      if (name != null && !name.trim().isEmpty())
+         result += "name: " + name;
+      return result;
+   }
+}
